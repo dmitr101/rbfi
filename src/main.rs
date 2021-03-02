1cbfe39367d246b93b7b83bd8edc0dca9ae70515
Shynkar Dmytro <d.shynkar@coworking.local>
2021-03-02 16:14:57 +0200

    Refactored interpreter.

--- src/main.rs
+++ src/main.rs
@@ -8,159 +8,176 @@ const MAX_MEM_CELL_COUNT : usize = 1024;
 
 struct InterpreterContext  {
     memory : [i8; MAX_MEM_CELL_COUNT],
-    index : usize,
+    mem_cell_index : usize,
+    cycle_stack : Vec<usize>,
+    script_cursor : usize,
 }
 
-impl InterpreterContext {
-     fn new() -> InterpreterContext {
-        InterpreterContext {
-            memory : [0; MAX_MEM_CELL_COUNT],
-            index : 0
-        }
-    }
+#[derive(Debug)]
+struct MemCellRelatedError {
+    cell_index: usize,
+    script_pos: usize,
+}
+
+#[derive(Debug)]
+struct PositionalError {
+    script_pos : usize,
 }
 
 enum ExecutionError{
-    MemCellAccessOutOfBounds(usize, usize),
-    MemCellValueOverflow(usize, usize),
-    MemCellValueUnderflow(usize, usize),
-    UnexpectedCycleEnd(usize),
-    IOError,
+    MemCellAccessOutOfBounds(MemCellRelatedError),
+    MemCellValueOverflow(MemCellRelatedError),
+    MemCellValueUnderflow(MemCellRelatedError),
+    UnexpectedCycleEnd(PositionalError),
+    IOError(PositionalError),
 }
 
-//TODO: Make it easier to use
-fn exec_checked<F>(op : F, context : &mut InterpreterContext, pos : usize, err : ExecutionError) -> Result<(), ExecutionError> 
-    where F: FnOnce(&mut InterpreterContext) -> bool {
-    if context.index >= MAX_MEM_CELL_COUNT { 
-        return Err(ExecutionError::MemCellAccessOutOfBounds(context.index, pos));
+impl InterpreterContext {
+    fn new() -> InterpreterContext {
+        InterpreterContext {
+            memory : [0; MAX_MEM_CELL_COUNT],
+            mem_cell_index : 0,
+            cycle_stack : Vec::new(),
+            script_cursor : 0,
+        }
     }
 
-    if op(context) {
-        return Ok(())
+    fn mem_cell_err(&self) -> MemCellRelatedError {
+        MemCellRelatedError{
+            cell_index: self.mem_cell_index,
+            script_pos: self.script_cursor,
+        }
     }
-    Err(err)
+
+    fn pos_err(&self) -> PositionalError {
+        PositionalError{
+            script_pos: self.script_cursor,
+        }
+    }
+
+    fn check_out_of_bounds_access(&self) -> Result<(), ExecutionError> {
+        if self.mem_cell_index >= MAX_MEM_CELL_COUNT {
+            return Err(ExecutionError::MemCellAccessOutOfBounds(self.mem_cell_err()));
+        }
+        Ok(())
+    }
+
+    fn inc_checked(&mut self) -> Result<(), ExecutionError> {
+        self.check_out_of_bounds_access()?;
+        let cur_cell = &mut self.memory[self.mem_cell_index];
+        match cur_cell.checked_add(1) {
+            Some(v) => *cur_cell = v,
+            None => return Err(ExecutionError::MemCellValueOverflow(self.mem_cell_err()))
+        }
+        Ok(())
+    }
+    
+    fn dec_checked(&mut self) -> Result<(), ExecutionError> {
+        self.check_out_of_bounds_access()?;
+        let cur_cell = &mut self.memory[self.mem_cell_index];
+        match cur_cell.checked_sub(1) {
+            Some(v) => *cur_cell = v,
+            None => return Err(ExecutionError::MemCellValueUnderflow(self.mem_cell_err()))
+        }
+        Ok(())
+    }
+    
+    fn put_checked(&self) -> Result<(), ExecutionError> {
+        self.check_out_of_bounds_access()?;
+        print!("{}", self.memory[self.mem_cell_index] as u8 as char);
+        Ok(())
+    }
+
+    fn get_checked(&mut self) -> Result<(), ExecutionError> {
+        self.check_out_of_bounds_access()?;   
+        match get_one_byte_from_stdin() {
+            Some(c) => self.memory[self.mem_cell_index] = c,
+            None => return Err(ExecutionError::IOError(self.pos_err()))
+        }
+        Ok(())
+    }
+    
+    fn execute(&mut self, script : &str) -> Result<(), ExecutionError>{
+        loop {
+            match script.as_bytes()[self.script_cursor] {
+                b'>' => self.mem_cell_index += 1,
+                b'<' => self.mem_cell_index -= 1,
+                b'+' => self.inc_checked()?,
+                b'-' => self.dec_checked()?,
+                b'.' => self.put_checked()?,
+                b',' => self.get_checked()?,
+                b'[' => {
+                    self.check_out_of_bounds_access()?;
+                    if self.memory[self.mem_cell_index] != 0 {
+                        self.cycle_stack.push(self.script_cursor);
+                    } else {
+                        match script.split_at(self.script_cursor).1.find(']') {
+                            Some(offset) => self.script_cursor += offset,
+                            None => self.script_cursor = script.len(),
+                        }
+                    }
+                },
+                b']' => {
+                    if !self.cycle_stack.is_empty() {
+                        self.script_cursor = self.cycle_stack.pop().unwrap() - 1;
+                    } else {
+                        return Err(ExecutionError::UnexpectedCycleEnd(self.pos_err()));
+                    }
+                },
+                _ => {}
+            }
+    
+            self.script_cursor += 1;
+            if self.script_cursor >= script.len() {
+                break;
+            }
+        }
+        Ok(())
+    }
+
+    fn execute_from_start(&mut self, script : &str) -> Result<(), ExecutionError> {
+        self.script_cursor = 0;
+        self.execute(script)
+    }
 }
 
-fn inc_checked(context : &mut InterpreterContext, pos : usize) -> Result<(), ExecutionError> {
-    if context.index >= MAX_MEM_CELL_COUNT {
-        return Err(ExecutionError::MemCellAccessOutOfBounds(context.index, pos));
-    }
-    if context.memory[context.index] == i8::MAX {
-        return Err(ExecutionError::MemCellValueOverflow(context.index, pos));
-    }
-
-    context.memory[context.index] += 1;
-    Ok(())
-}
-
-fn dec_checked(context : &mut InterpreterContext, pos : usize) -> Result<(), ExecutionError> {
-    if context.index >= MAX_MEM_CELL_COUNT {
-        return Err(ExecutionError::MemCellAccessOutOfBounds(context.index, pos));
-    }
-    if context.memory[context.index] == i8::MIN {
-        return Err(ExecutionError::MemCellValueUnderflow(context.index, pos));
-    }
-
-    context.memory[context.index] -= 1;
-    Ok(())
-}
-
-fn put_checked(context : &mut InterpreterContext, pos : usize) -> Result<(), ExecutionError> {
-    if context.index >= MAX_MEM_CELL_COUNT {
-        return Err(ExecutionError::MemCellAccessOutOfBounds(context.index, pos));
-    }
-
-    print!("{}", context.memory[context.index] as u8 as char);
-    Ok(())
-}
-
-fn get_checked(context : &mut InterpreterContext, pos : usize) -> Result<(), ExecutionError> {
-    if context.index >= MAX_MEM_CELL_COUNT {
-        return Err(ExecutionError::MemCellAccessOutOfBounds(context.index, pos));
-    }
-
-    match io::stdin()
+fn get_one_byte_from_stdin() -> Option<i8> {
+    io::stdin()
         .bytes()
         .next()
         .and_then(|result| result.ok())
-        .map(|byte| byte as i8) {
-        Some(c) => context.memory[context.index] = c,
-        None => return Err(ExecutionError::IOError)
-    }
-    Ok(())
-}
-
-fn execute(script : &str, context : &mut InterpreterContext) -> Result<(), ExecutionError> {
-    let mut script_cursor : usize = 0;
-    let mut cycle_stack : Vec<usize> = Vec::new();
-    loop {
-        match script.as_bytes()[script_cursor] {
-            b'>' => context.index += 1,
-            b'<' => context.index -= 1,
-            b'+' => inc_checked(context, script_cursor)?,
-            b'-' => dec_checked(context, script_cursor)?,
-            b'.' => put_checked(context, script_cursor)?,
-            b',' => get_checked(context, script_cursor)?,
-            b'[' => {
-                if context.memory[context.index] != 0 {
-                    cycle_stack.push(script_cursor);
-                } else {
-                    match script.split_at(script_cursor).1.find(']') {
-                        Some(offset) => script_cursor += offset,
-                        None => script_cursor = script.len(),
-                    }
-                }
-            },
-            b']' => {
-                if !cycle_stack.is_empty() {
-                    script_cursor = cycle_stack.pop().unwrap() - 1;
-                } else {
-                    return Err(ExecutionError::UnexpectedCycleEnd(script_cursor));
-                }
-            },
-            _ => {}
-        }
-
-        script_cursor += 1;
-        if script_cursor >= script.len() {
-            break;
-        }
-    }
-    Ok(())
+        .map(|byte| byte as i8)
 }
 
 fn main() {
-    let args : Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("Interpreter expects one argument only - a file with a script!");
-        return;
-    }
+    match env::args().nth(1) {
+        Some(arg) => {
+            let script_str = {
+                let script_file = match fs::File::open(arg.as_str()) {
+                    Ok(file) => file,
+                    Err(e) => {
+                        println!("Couldn't open file {}, error: {}", arg, e);
+                        return;
+                    }
+                };
+                let mut reader = io::BufReader::new(script_file);
+                let mut content = String::new();
+                reader.read_to_string(&mut content).unwrap();
+                content
+            };
 
-    let arg_path = args[1].as_str();
-    let script_file = match fs::File::open(arg_path) {
-        Ok(file) => file,
-        Err(e) => {
-            println!("Couldn't open file {}, error: {}", arg_path, e);
-            return;
+            let mut context = InterpreterContext::new();
+            match context.execute_from_start(script_str.as_str()) {
+                Ok(()) => println!("Executted successfully. Exitting..."),
+                Err(e) => match e {
+                    ExecutionError::MemCellAccessOutOfBounds(err) => println!("MemCellAccessOutOfBounds Error. {:?}", err),
+                    ExecutionError::MemCellValueOverflow(err) => println!("MemCellValueOverflow Error. {:?}", err),
+                    ExecutionError::MemCellValueUnderflow(err) => println!("MemCellValueUnderflow Error. {:?}", err),
+                    ExecutionError::UnexpectedCycleEnd(err) => println!("UnexpectedCycleEnd Error. {:?}", err),
+                    ExecutionError::IOError(err) => println!("IOError Error. Couldn't read from the terminal. {:?}", err),
+                }
+            }
         }
-    };
-
-    let script_str = {
-        let mut reader = io::BufReader::new(script_file);
-        let mut content = String::new();
-        reader.read_to_string(&mut content).unwrap();
-        content
-    };
-
-    let mut context = InterpreterContext::new();
-    match execute(script_str.as_str(), &mut context) {
-        Ok(()) => println!("Executted successfully. Exitting..."),
-        Err(e) => match e {
-            ExecutionError::MemCellAccessOutOfBounds(s1,s2) => println!("Error. Overflowing {}", s1),
-            ExecutionError::MemCellValueOverflow(s1, s2) => println!("Error. Overflowing {}", s1),
-            ExecutionError::MemCellValueUnderflow(s1, s2) => println!("Error. Overflowing {}", s1),
-            ExecutionError::UnexpectedCycleEnd(s) => println!("Error. Unexpected cycle end at position {}", s),
-            ExecutionError::IOError => println!("Error. Couldn't read from the terminal."),
-        }
+        None => println!("Error: Expected to get a script filename as an argument!")
     }
 }
